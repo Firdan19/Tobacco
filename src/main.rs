97e10894f00e6f901f0f564e3afd5ae79014b7ab
Firdan19 <171222908+Firdan19@users.noreply.github.com>
2026-06-18 12:43:27 +0700

    Tambahkan bootstrap long mode Multiboot2

--- src/main.rs
+++ src/main.rs
@@ -10,19 +10,94 @@ mod memory;
 
 core::arch::global_asm!(
     r#"
-    .section .text
+    .att_syntax prefix
+    .section .text.boot, "ax"
+    .code32
     .global _start
 _start:
     cli
-    lea rsp, [rip + stack_top]
-    xor rbp, rbp
+    movl $stack_top, %esp
+    xorl %ebp, %ebp
+
+    movl $p2_table, %edi
+    xorl %eax, %eax
+    movl $512, %ecx
+1:
+    movl %eax, %ebx
+    shll $21, %ebx
+    orl $0x83, %ebx
+    movl %ebx, (%edi)
+    movl $0, 4(%edi)
+    addl $8, %edi
+    incl %eax
+    loop 1b
+
+    movl $p2_table, %eax
+    orl $0x03, %eax
+    movl %eax, p3_table
+    movl $0, p3_table+4
+
+    movl $p3_table, %eax
+    orl $0x03, %eax
+    movl %eax, p4_table
+    movl $0, p4_table+4
+
+    movl $p4_table, %eax
+    movl %eax, %cr3
+
+    movl %cr4, %eax
+    orl $0x20, %eax
+    movl %eax, %cr4
+
+    movl $0xC0000080, %ecx
+    rdmsr
+    orl $0x100, %eax
+    wrmsr
+
+    lgdt gdt_descriptor
+
+    movl %cr0, %eax
+    orl $0x80000001, %eax
+    movl %eax, %cr0
+
+    ljmp $0x08, $long_mode_start
+
+    .code64
+long_mode_start:
+    movw $0x10, %ax
+    movw %ax, %ds
+    movw %ax, %es
+    movw %ax, %ss
+    movw %ax, %fs
+    movw %ax, %gs
+
+    leaq stack_top(%rip), %rsp
+    xorq %rbp, %rbp
     call kernel_main
 
 .halt:
     hlt
     jmp .halt
 
+    .section .rodata
+    .align 8
+gdt:
+    .quad 0x0000000000000000
+    .quad 0x00af9a000000ffff
+    .quad 0x00cf92000000ffff
+gdt_end:
+gdt_descriptor:
+    .word gdt_end - gdt - 1
+    .long gdt
+
     .section .bss
+    .align 4096
+p4_table:
+    .skip 4096
+p3_table:
+    .skip 4096
+p2_table:
+    .skip 4096
     .align 16
 stack_bottom:
     .skip 16384
